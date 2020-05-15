0b192f067eef4a84616b4bc93a68be696c7179dc
MohamedUIEXP <mohamed.p@foolproof.co.uk>
2020-05-15 19:57:08 +0100

    added journals page

--- client__libraries/js/rics.js
+++ client__libraries/js/rics.js
@@ -31,10 +31,12 @@ function gotoSlide(n) {
 function showDivs(n) {
       var i;
       var x = document.getElementsByClassName("slider__img");
-      if (n > x.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = x.length}
-      for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";  
+      if (x.length) {
+            if (n > x.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = x.length}
+            for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+            }
+            x[slideIndex-1].style.display = "block";  
       }
-      x[slideIndex-1].style.display = "block";  
 }
