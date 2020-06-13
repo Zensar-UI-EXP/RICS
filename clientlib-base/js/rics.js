abfe3c4e76b1e528fb054196af953dde95ca81ca
MohamedUIEXP <mohamed.p@foolproof.co.uk>
2020-06-13 01:44:53 +0100

    search results page and no search results page

--- clientlib-base/js/rics.js
+++ clientlib-base/js/rics.js
@@ -403,4 +403,131 @@ function onlyAlphabets(e, t) {
       catch (err) {
           console.log(err.Description);
       }
-  }
+}
+
+function checkNAddRemoveClass(parentNode, checkClass, addClass) {
+      if (parentNode.classList.contains(checkClass)) {
+            if (parentNode.classList.contains(addClass)){
+                  parentNode.classList.remove(addClass);
+            } else {
+                  parentNode.classList.add(addClass);
+            }
+            
+      } else {
+            parentNode = parentNode.parentNode;
+            checkNAddRemoveClass(parentNode, checkClass, addClass);
+      }
+}
+
+function expandCollapse(thisElement) {
+      checkNAddRemoveClass(thisElement, "filtersection__options", "filtersection--collapsed");
+}
+
+var dropdownElement = document.querySelector(".dropdown");
+if (dropdownElement){
+      dropdownElement.onclick = function(e){
+            for (var i = 0; i < e.srcElement.childNodes.length; i++){
+                  if (e.srcElement.childNodes[i].nodeName.toLowerCase() == "select"){
+                        e.srcElement.childNodes[i].select();
+                  }
+            }
+      }
+}
+
+var selectedFilterList = [];
+var thisElementList = [];
+function selectFilter(thisElement, removeValue) {
+      var filterText = "";
+      var inputIndex = -1;
+      var removeValue = document.body.clientWidth < 768 ? true : removeValue;
+      for (var i = 0; i < thisElement.childNodes.length; i++){
+            if (thisElement.childNodes[i].offsetWidth || document.body.clientWidth < 768) {
+                  if (thisElement.childNodes[i].name && thisElement.childNodes[i].nodeName.toLowerCase() === "input"){
+                        inputIndex = i;
+                        filterText += thisElement.childNodes[i].name + ':';
+                  } else if (thisElement.childNodes[i].innerText) {
+                        filterText += thisElement.childNodes[i].innerText;
+                  }
+            }
+      }
+      var index = selectedFilterList.indexOf(filterText);
+      if (index === -1) {
+            selectedFilterList.push(filterText);
+            thisElement.childNodes[inputIndex].checked = true;
+            thisElementList.push(thisElement);
+      } else {
+            if (removeValue) {
+                  selectedFilterList.splice(index, 1);
+                  thisElement.childNodes[inputIndex].checked = false;
+                  thisElementList.splice(index, 1);
+            } else {
+                  selectedFilterList[index] = "";
+                  thisElement.childNodes[inputIndex].checked = false;
+                  selectedFilterList[index] = "";            
+            }
+      }
+      generateFilterHTML();
+}
+
+function generateFilterHTML () {
+      if (document.body.clientWidth > 768) {
+            appliedFilterElement = document.getElementsByClassName("filtersection--appliedfilterdesktop"); 
+      } else {
+            appliedFilterElement = document.getElementsByClassName("filtersection--appliedfiltermobile");
+      }
+      var clearAllFilterElement = document.getElementsByClassName("filtersection__clearall");
+      var closeElement = document.getElementsByClassName("filtersection--onlyclose");
+      var applyCloseElement = document.getElementsByClassName("filtersection--applyclose");
+      if (selectedFilterList.length) {
+            appliedFilterElement[0].style.display = "block";
+            clearAllFilterElement[0].style.display = "block";
+            closeElement[0].style.display = "none";
+            applyCloseElement[0].style.display = "block";
+            applyCloseElement[1].style.display = "block";
+      } else {
+            appliedFilterElement[0].style.display = "none";
+            clearAllFilterElement[0].style.display = "none";
+            closeElement[0].style.display = "block";
+            applyCloseElement[0].style.display = "none";
+            applyCloseElement[1].style.display = "none";
+      }
+      var filterHTML = "";
+      for (var i = 0; i < selectedFilterList.length; i++){
+            if (selectedFilterList[i]) {
+                  var splitValue = selectedFilterList[i].split(":");
+                  filterHTML += "<li class='filtersection__appliedfilterlist'>";
+                  filterHTML += "<label class='filtersection__appliedfilterkey'>" + splitValue[0] + ":</label>";
+                  filterHTML += "<strong class='filtersection__appliedfiltervalue'>" + splitValue[1] + "</strong>";
+                  filterHTML += "<a href='javascript:void(0)' class='filtersection__clearfilter' title='Clear "+ selectedFilterList[i] +"' onclick='clearFilter("+ i +", true)'></a></li>";
+            }
+      }
+      appliedFilterElement[0].innerHTML = filterHTML;
+}
+
+function clearFilter (index, removeValue) {
+      selectFilter(thisElementList[index], removeValue);
+}
+
+function clearAllFilter () {
+      for (var i = 0; i < selectedFilterList.length; i++){
+            clearFilter(i, false);
+      }
+      selectedFilterList = [];
+      thisElementList = [];
+      generateFilterHTML();
+}
+
+function openFilter(thisElement){
+      thisElement.classList.add("special--refineactive");
+      var filterElement = document.getElementsByClassName("filtersection");
+      filterElement[0].style.display = "block";
+      document.body.classList.add("fixed--layout");
+}
+
+function closeFilter(){
+      var refineElement = document.getElementsByClassName("special--refineactive");
+      refineElement[0].classList.remove("special--refineactive");
+      var filterElement = document.getElementsByClassName("filtersection");
+      filterElement[0].style.display = "none";
+      document.body.classList.remove("fixed--layout");
+}
